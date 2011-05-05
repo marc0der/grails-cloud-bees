7e27337ffd33eb2a62ec04c05c4453e5b43a3d9d
Marco Vermeulen <vermeulen.mp@gmail.com>
2011-05-05 16:11:55 +0100

    Implement db:delete cli script.

---
+++ scripts/BeesDbDelete.groovy
@@ -0,0 +1,33 @@
+import net.stax.api.StaxClientException
+
+includeTargets << grailsScript("Init")
+includeTargets << new File("${basedir}/scripts/_CheckConfig.groovy")
+includeTargets << new File("${basedir}/scripts/_StaxHelper.groovy")
+includeTargets << new File("${basedir}/scripts/_BeesCommon.groovy")
+
+USAGE = '''
+grails bees-db-delete <dbId>
+'''
+
+target(beesDbDelete: "Delete a MySQL database.") {
+	depends(checkConfig, prepareClient)
+	
+	String dbId = getRequiredArg(0)
+	
+	def response
+	try {
+		response = staxClient.databaseDelete(dbId)
+		
+	} catch (StaxClientException sce) {
+		printSeparator()
+		println "Database not deleted: $sce.message"
+		printSeparator()
+		exit(0)
+	}
+	
+	printSeparator()
+	println "Database $dbId deleted successfully: $response.deleted"
+	printSeparator()
+}
+
+setDefaultTarget(beesDbDelete)
