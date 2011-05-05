f5c55e6f1530bd2b32c8998a988c2081caacdbe8
Marco Vermeulen <vermeulen.mp@gmail.com>
2011-05-05 16:11:34 +0100

    Implement db:info cli script.

---
+++ scripts/BeesDbInfo.groovy
@@ -0,0 +1,40 @@
+import net.stax.api.StaxClientException
+
+includeTargets << grailsScript("Init")
+includeTargets << new File("${basedir}/scripts/_CheckConfig.groovy")
+includeTargets << new File("${basedir}/scripts/_StaxHelper.groovy")
+includeTargets << new File("${basedir}/scripts/_BeesCommon.groovy")
+
+USAGE = '''
+grails bees-db-info <dbId>
+'''
+
+target(beesDbInfo: "Returns information about connecting to a database.") {
+	depends(checkConfig, prepareClient)
+	
+	String dbId = getRequiredArg()
+	def info
+	try {
+		info = staxClient.databaseInfo(dbId, true)
+		
+	} catch (StaxClientException sce) {
+		printSeparator()
+		println "Error: $sce.message"
+		printSeparator()
+		exit(0)
+	}
+	
+	printSeparator()
+	println "Database Name : $info.name"
+	println "      created : $info.created"
+	println "        owner : $info.owner"
+	println "     username : $info.username"
+	println "     password : $info.password"
+	println "       master : $info.master"
+	println "         port : $info.port"
+	println "       slaves : $info.slaves"
+	println "       status : $info.status"
+	printSeparator()
+}
+
+setDefaultTarget(beesDbInfo)
