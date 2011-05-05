7f25d9605e486c59bff76650c95189a0258718d0
Marco Vermeulen <vermeulen.mp@gmail.com>
2011-05-05 17:38:59 +0100

    Reference cloudBeesPluginDir instead of baseDir in Scripts.

--- scripts/BeesDbDelete.groovy
+++ scripts/BeesDbDelete.groovy
@@ -1,9 +1,9 @@
 import net.stax.api.StaxClientException
 
 includeTargets << grailsScript("Init")
-includeTargets << new File("${basedir}/scripts/_CheckConfig.groovy")
-includeTargets << new File("${basedir}/scripts/_StaxHelper.groovy")
-includeTargets << new File("${basedir}/scripts/_BeesCommon.groovy")
+includeTargets << new File("${cloudBeesPluginDir}/scripts/_CheckConfig.groovy")
+includeTargets << new File("${cloudBeesPluginDir}/scripts/_StaxHelper.groovy")
+includeTargets << new File("${cloudBeesPluginDir}/scripts/_BeesCommon.groovy")
 
 USAGE = '''
 grails bees-db-delete <dbId>
