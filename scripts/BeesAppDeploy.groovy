2e0dff02e2a7580cd1fc47ac744c8c53a2177985
Marco Vermeulen <vermeulen.mp@gmail.com>
2011-05-06 11:45:23 +0100

    Added the application deploy script.

---
+++ scripts/BeesAppDeploy.groovy
@@ -0,0 +1,37 @@
+import net.stax.api.HashWriteProgress
+import net.stax.api.StaxClientException
+
+includeTargets << grailsScript("Init")
+includeTargets << new File("${cloudBeesPluginDir}/scripts/_CheckConfig.groovy")
+includeTargets << new File("${cloudBeesPluginDir}/scripts/_StaxHelper.groovy")
+includeTargets << new File("${cloudBeesPluginDir}/scripts/_BeesCommon.groovy")
+
+USAGE = '''
+grails bees-app-deploy <appId> "<message>" WAR_ARCHIVE_FILE
+'''
+
+target(beesAppDeploy: "Deploy a new version of an application using a WAR archive file.") {
+	depends(checkConfig, prepareClient)
+	
+	String appId = getRequiredArg(0)
+	String message = getRequiredArg(1)
+	String war = getRequiredArg(2)
+	
+	def response
+	def progress = new HashWriteProgress()
+	try {
+		response = staxClient.applicationDeployWar(appId, null, message, war, null, true, progress)
+		
+	} catch (StaxClientException sce) {
+		printSeparator()
+		println "Database not created: $sce.message"
+		printSeparator()
+		exit(0)
+	}
+	
+	printSeparator()
+	println "Application uploaded successfully to: $response.url"
+	printSeparator()
+}
+
+setDefaultTarget(beesAppDeploy)
