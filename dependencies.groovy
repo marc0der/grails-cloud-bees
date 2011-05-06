3ac99478352f25b8fe99f359897a380b52f28ca1
Marco Vermeulen <vermeulen.mp@gmail.com>
2011-05-06 16:27:30 +0100

    Use cloudbees-api-client 0.8.0 from mvn. Removed jar file from lib folder.

--- dependencies.groovy
+++ dependencies.groovy
@@ -22,6 +22,7 @@ grails.project.dependency.resolution = {
 	dependencies {
 		runtime('commons-httpclient:commons-httpclient:3.1')
 		runtime('com.thoughtworks.xstream:xstream:1.3.1')
+		runtime('com.cloudbees:cloudbees-api-client:0.8.0')
 	}
 
 	plugins {
