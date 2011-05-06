2c206dab3e24dd4dc2f90cc952e4fffab5022875
Marco Vermeulen <vermeulen.mp@gmail.com>
2011-05-06 16:32:44 +0100

    Allow for help parameter on scripts that take one or more parameter.

--- scripts/_BeesCommon.groovy
+++ scripts/_BeesCommon.groovy
@@ -1,7 +1,7 @@
 getRequiredArg = { int index = 0 ->
 	def argsList = argsMap.params
 	String value = argsList[index]
-	if (value) {
+	if (value && (value != 'help')) {
 		return value
 	}
 	println "\nUsage (optionals in square brackets):\n$USAGE"
