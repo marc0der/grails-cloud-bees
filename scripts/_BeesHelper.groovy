ad16183943ac8df519d357f121ae3bcbe76f91ed
Marco Vermeulen <marco@hashcode.co>
2011-12-06 08:30:46 +0000

    Set API URL to sensible default with option to override

--- scripts/_BeesHelper.groovy
+++ scripts/_BeesHelper.groovy
@@ -3,8 +3,9 @@ import com.cloudbees.api.BeesClientException
 
 target(prepareClient: "Prepare the BeesClient"){
 	event "StatusUpdate", ["Preparing the BeesClient"]
+	def apiURL = config.cloudbees.api.url ?: 'https://api.cloudbees.com/api'
 	beesClient = new BeesClient(
-		config.cloudbees.api.url, 
+		apiURL, 
 		config.cloudbees.api.key, 
 		config.cloudbees.api.secret, 
 		'xml', '1.0'
--- scripts/_CheckConfig.groovy
+++ scripts/_CheckConfig.groovy
@@ -4,10 +4,9 @@ target(checkConfig: "Ensure that the configuration is set."){
 	depends(compile, createConfig)
 
 	event("StatusUpdate", ["Checking CloudBees config"])
-	if(!(config.cloudbees.api.url && config.cloudbees.api.key && config.cloudbees.api.secret)){
+	if(!(config.cloudbees.api.key && config.cloudbees.api.secret)){
 		event "StatusError",  ["CloudBees Plugin not configured!"]
 		event "StatusError",  ["Please add appropriate configuration for:"]
-		if(!config.cloudbees.api.url) event("StatusError",  ["The API url    : cloudbees.api.url"])
 		if(!config.cloudbees.api.key) event("StatusError",  ["The API key    : cloudbees.api.key"])
 		if(!config.cloudbees.api.secret) event("StatusError",  ["The API secret : cloudbees.api.secret"])
 		exit(0)
