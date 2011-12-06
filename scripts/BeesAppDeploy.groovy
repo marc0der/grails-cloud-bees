92684351c5ced96f5ea021fbb56eabb1f82a7581
Marco Vermeulen <marco@hashcode.co>
2011-12-06 15:25:21 +0000

    Improved user experience for bees-app-deploy script.

--- scripts/BeesAppDeploy.groovy
+++ scripts/BeesAppDeploy.groovy
@@ -1,3 +1,5 @@
+import grails.util.Metadata;
+
 import com.cloudbees.api.HashWriteProgress
 
 includeTargets << grailsScript("Init")
@@ -6,28 +8,42 @@ includeTargets << new File("${cloudBeesPluginDir}/scripts/_BeesHelper.groovy")
 includeTargets << new File("${cloudBeesPluginDir}/scripts/_BeesCommon.groovy")
 
 USAGE = '''
-grails bees-app-deploy <appId> <tag> <war>
-	appId : the application id (in the form user/appname)
-	tag   : the release tag or version
-	war   : the path to the warfile (absolute or relative)
+grails bees-app-deploy [tag]
+	tag : optional release tag (defaults to application version)
 '''
 
 target(beesAppDeploy: "Deploy a new version of an application using a WAR archive file.") {
 	depends(checkConfig, prepareClient)
 	
-	String appId = getRequiredArg(0)
-	if(!appId) return
+	if(argsMap.params[0] == 'help'){
+		event "StatusFinal", ["\nUsage (optionals in square brackets):\n$USAGE"]
+		return
+	}
 	
-	String tag = getRequiredArg(1)
-	if(!tag) return
+	if(!config.cloudbees.account){
+		event "StatusError", ["Apropriate configuration for Account should be added: config.cloudbees.account"]
+		return
+	}
 	
-	String war = getRequiredArg(2)
-	if(!war) return
+	String account = config.cloudbees.account
+	String appName = Metadata.current.'app.name'
+	String appId = config.cloudbees.appid ?: appName
+	String appVersion = Metadata.current.'app.version'
+	String tag = argsMap.params[0] ?: appVersion
+	
+	String application = "${account}/${appId}"
+	
+	String warName = "${grailsSettings.baseDir}/target/${appName}-${appVersion}.war"
+	File warFile = new File(warName)
+	if(! (warFile.exists() && warFile.file) ){
+		event "StatusError", ["WAR file is not build! First issue the grails war command before deploying."]
+		return
+	}
 	
 	def response
 	def progress = new HashWriteProgress()
 	try {
-		response = beesClient.applicationDeployWar(appId, null, tag, war, null, true, progress)
+		response = beesClient.applicationDeployWar(application, null, tag, warName, null, true, progress)
 		
 	} catch (Exception e) {
 		dealWith e
