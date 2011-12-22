8366c21115556c1105203395707c83e9cfb69934
Marco Vermeulen <marco@hashcode.co>
2011-12-22 14:06:21 +0000

    Moved dependencies.groovy to BuildConfig.groovy

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,34 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        mavenLocal()
+        mavenCentral()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    
+	dependencies {
+		runtime('com.cloudbees:cloudbees-api-client:1.1.1'){
+			excludes 'stax-api'
+		}
+		runtime 'xmlpull:xmlpull:1.1.3.1'
+	}
+}
+
